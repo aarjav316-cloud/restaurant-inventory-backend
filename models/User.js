75035c7392d94c80862952b312ae9c978fe4c3a1
Aarjav jain <aarjav316@gmail.com>
2026-03-16 21:50:16 +0530

    user model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:["admin" , "chef" , "staff"],
+        required:true
+    }
+},{timestamps:true})
+
+userSchema.index({email:1} , {unique:true})
+
+const User = mongoose.model("User" , userSchema)
+
+export default User;
+
+
--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.json())
 app.use(cors())
 app.use(morgan("dev"))
 
-connectDB()
-connectRedis()
+connectDB();
+connectRedis();
 
 app.get("/" , (req,res) => {
     res.send(`server running on port ${PORT}`)
