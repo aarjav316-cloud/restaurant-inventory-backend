7fcd7be37d915bc5e10aa76d8d1b271cec7f763d
Aarjav jain <aarjav316@gmail.com>
2026-03-16 21:21:12 +0530

    basic setup of redis and mongodb

---
+++ config/redis.js
@@ -0,0 +1,28 @@
+import { createClient } from "redis";
+
+let redisClient
+
+const connectRedis = async () => {
+    try {
+
+        redisClient = createClient({
+            url:process.env.REDIS_URL
+        })
+
+        redisClient.on("error" , (err) => {
+            console.log("Redis Error" , err)
+        })
+
+        await redisClient.connect()
+
+        console.log("Redis connected")
+        
+    } catch (error) {
+        return res.json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+
---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev"))
+
+app.get("/" , (req,res) => {
+    res.send(`server running on port ${PORT}`)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT , () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
