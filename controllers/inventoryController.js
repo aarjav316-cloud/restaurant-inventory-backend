d16491ef23df5ed6ab22e5354590d0217179bd62
Aarjav jain <aarjav316@gmail.com>
2026-03-17 10:10:14 +0530

    getInventory controller

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -38,7 +38,40 @@ export const addInventory = async(req,res) => {
 
 
 
+export const getInventory = async (req,res) => {
+    try {
 
+        const cachedData = await redisClient.get("inventory:all")
+
+        if(cachedData){
+            return res.status(200).json({
+                success:true,
+                source:"redis",
+                data:JSON.parse(cachedData)
+            })
+        }
+
+        const inventory = await Inventory.find()
+
+        await redisClient.set(
+            "inventory:all",
+            JSON.stringify(inventory),
+            {EX:60}
+        )
+
+        return res.status(200).json({
+            success:true,
+            source:"database",
+            data:inventory
+        })
+        
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
 
 
 
